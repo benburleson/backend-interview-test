4b90526f5e519b0283bc06d18d6f0ad63fc68113
Ben Burleson <ben.burleson@gmail.com>
2014-03-21 11:46:45 -0700

    Fix indenting
    And a missing semicolon.

--- backend_test.spec.js
+++ backend_test.spec.js
@@ -1,36 +1,36 @@
 var Converter = require('./backend');
 
 describe("Converter", function() {
-  	var converter = new Converter();
+    var converter = new Converter();
 
-  	var expanded = {
-    	'one': { 
+    var expanded = {
+        'one': { 
             'two': 3,
             'four': [ 5,6,7]
         },
-    	'eight': {
+        'eight': {
             'nine': {
-                    'ten':11
-                }
+                'ten':11
+            }
         }
-	};
+    };
 
-	var flattened = {
-	    'one/two':3,
-	    'one/four/0':5,
-	    'one/four/1':6,
-	    'one/four/2':7,
-	    'eight/nine/ten':11
-	}
+    var flattened = {
+        'one/two':3,
+        'one/four/0':5,
+        'one/four/1':6,
+        'one/four/2':7,
+        'eight/nine/ten':11
+    };
 
-  	it("flatten", function() {
-  	    var result = converter.flatten(expanded);
-    	expect(result).toEqual(flattened);
-  	});
+    it("flatten", function() {
+        var result = converter.flatten(expanded);
+        expect(result).toEqual(flattened);
+    });
 
-  	it("expand", function() {
-  	    var result = converter.expand(flattened);
-    	expect(result).toEqual(expanded);
-  	});
+    it("expand", function() {
+        var result = converter.expand(flattened);
+        expect(result).toEqual(expanded);
+    });
 
 });
